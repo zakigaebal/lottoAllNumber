fca783da606ba9406b103e28f458583ffebb2b9b
zakigaebal <86054791+zakigaebal@users.noreply.github.com>
2022-04-01 17:00:39 +0900

    보간식과 string,format 속도 비교

--- WindowsFormsApp7/Form1.Designer.cs
+++ WindowsFormsApp7/Form1.Designer.cs
@@ -33,15 +33,18 @@
 			this.button4 = new System.Windows.Forms.Button();
 			this.textBox2 = new System.Windows.Forms.TextBox();
 			this.button5 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(12, 6);
+			this.button1.Location = new System.Drawing.Point(12, 68);
 			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(134, 28);
+			this.button1.Size = new System.Drawing.Size(236, 28);
 			this.button1.TabIndex = 0;
-			this.button1.Text = "로또조합번호가져오기";
+			this.button1.Text = "1. 보간식과 string.format 속도 비교하기";
 			this.button1.UseVisualStyleBackColor = true;
 			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
@@ -78,11 +81,43 @@
 			this.button5.Text = "지우기후 파일새로저장";
 			this.button5.UseVisualStyleBackColor = true;
 			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 16);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(65, 12);
+			this.label1.TabIndex = 6;
+			this.label1.Text = "시간대표시";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(12, 102);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(236, 26);
+			this.button2.TabIndex = 7;
+			this.button2.Text = "바이너리라이터쓰기";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click_1);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(336, 73);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 8;
+			this.button3.Text = "button3";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click_1);
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(492, 71);
+			this.ClientSize = new System.Drawing.Size(492, 144);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.label1);
 			this.Controls.Add(this.button5);
 			this.Controls.Add(this.textBox2);
 			this.Controls.Add(this.button4);
@@ -102,6 +137,9 @@
 		private System.Windows.Forms.Button button4;
 		private System.Windows.Forms.TextBox textBox2;
 		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
 	}
 }
 
--- WindowsFormsApp7/Form1.cs
+++ WindowsFormsApp7/Form1.cs
@@ -10,22 +10,24 @@ using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FS = System.IO.FileStream;
 
+/// <summary>
+/// 파일은 컴퓨터 저장 매체에 기록되는 데이터의 묶음입니다.
+/// 디렉토리는 파일이 우치하는 주소로 파일을 담는다는 의미에서 폴더라고 부릅니다.
+/// 
+/// </summary>
 namespace WindowsFormsApp7
 {
 	public partial class Form1 : Form
 	{
 		//랜덤함수 정의
 		Random random = new Random();
-
-
-
 		public Form1()
 		{
 			InitializeComponent();
 		}
-
-		void lottoAll()
+		void lottoFileAll()
 		{
 			//0번째부터
 			int i = 0;
@@ -37,7 +39,7 @@ namespace WindowsFormsApp7
 			int f = 1;
 			int g = 1;
 			// 6자리 배열 생성
-			int[] array1 = { a, b, c, d, f, g };
+			//int[] array1 = { a, b, c, d, f, g };
 
 			//로또는 1~45 자연수를 가지고 있음
 			//6개를 맞추는거임
@@ -45,75 +47,113 @@ namespace WindowsFormsApp7
 			//(45*44*43*42*41*40)/(6*5*4*3*2*1)계산식이 45C6임
 			//for문으로 123456을 가져옴 1 돌리고 2돌리고 3돌려서 마지막 40,41,42,43,44,45만 가지고 오면
 			// 갯수 8145060을 가져옴
-			Stopwatch stopwatch = new Stopwatch(); //객체 선언
-			stopwatch.Start(); // 시간측정 시작
-			for (array1[0] = 1; array1[0] <= 40; ++array1[0])
+
+			//// 복합 형식 지정 Format 
+		var stopWatch = Stopwatch.StartNew();  //객체 선언
+			stopWatch.Start(); // 시간측정 시작
+			for (a = 1; a <= 40; ++a)
 			{
-				for (array1[1] = array1[0] + 1; array1[1] <= 41; ++array1[1])
+				for (b = a + 1; b <= 41; ++b)
 				{
-					for (array1[2] = array1[1] + 1; array1[2] <= 42; ++array1[2])
+					for (c = b + 1; c <= 42; ++c)
 					{
-						for (array1[3] = array1[2] + 1; array1[3] <= 43; ++array1[3])
+						for (d = c + 1; d <= 43; ++d)
 						{
-							for (array1[4] = array1[3] + 1; array1[4] <= 44; ++array1[4])
+							for (f = d + 1; f <= 44; ++f)
 							{
-								for (array1[5] = array1[4] + 1; array1[5] <= 45; ++array1[5])
+								for (g = f + 1; g <= 45; ++g)
 								{
-									string lotto = string.Format("{0:#,00}", array1[0]) + string.Format("{0:#,00}", array1[1]) + string.Format("{0:#,00}", array1[2]) + string.Format("{0:#,00}", array1[3])
-											+ string.Format("{0:#,00}", array1[4]) + string.Format("{0:#,00}", array1[5]);
-
-							
-
-
-									List<LottoSerialize> ls = new List<LottoSerialize>();
-									LottoSerialize lottoSerialize = new LottoSerialize() { lottoNumber = lotto };
-									ls.Add(lottoSerialize);
-
-									//텍스트파일저장하기
-									fileText(lotto + "\n");
-
-								//바이너리파일저장하기
-									binaryAppendData("lot.bin", lotto + "\n");
-
-
-									Console.WriteLine(lotto);
-									i++;
-								
-
+									string lotto = string.Format("{0:#,00}", a) + string.Format("{0:#,00}", b) + string.Format("{0:#,00}", c) + string.Format("{0:#,00}", d)
+											+ string.Format("{0:#,00}", f) + string.Format("{0:#,00}", g);
+									//보간식을 쓰는게 더 빠를까?
+									string lotto2 = $"{a,0:#,00}{b,0:#,00}{c,0:#,00}{d,0:#,00}{f,0:#,00}{g,0:#,00}";
+									//Console.WriteLine(lotto);
 								}
-								
-
 							}
 						}
 					}
 				}
 			}
-			stopwatch.Stop(); //시간측정 끝
-			System.Console.WriteLine("time : " +
-			stopwatch.ElapsedMilliseconds + "ms");
-
-
-			MessageBox.Show("시간측정 : " + stopwatch.ElapsedMilliseconds + "ms");
-			//8145060
-			Console.WriteLine(i);
-
-		
-
+			stopWatch.Stop(); //시간측정 끝
+			System.Console.WriteLine("time : " + stopWatch.ElapsedMilliseconds / 1000d + "초");
+			//보간식을 쓰는게 더 빠를까?
+			// 문자열 보간 String Interpolation 
+			stopWatch = Stopwatch.StartNew();
+			for (a = 1; a <= 40; ++a)
+			{
+				for (b = a + 1; b <= 41; ++b)
+				{
+					for (c = b + 1; c <= 42; ++c)
+					{
+						for (d = c + 1; d <= 43; ++d)
+						{
+							for (f = d + 1; f <= 44; ++f)
+							{
+								for (g = f + 1; g <= 45; ++g)
+								{
+									string lotto2 = $"{a,0:#,00}{b,0:#,00}{c,0:#,00}{d,0:#,00}{f,0:#,00}{g,0:#,00}";
+									//Console.WriteLine(lotto2);
+								}
+							}
+						}
+					}
+				}
+			}
+			stopWatch.Stop();
+			System.Console.WriteLine("time : " + stopWatch.ElapsedMilliseconds/1000d + "초");
+			//보간식이 빠른가
+			//string.fromat이 빠른가
+			//보간식이 더 빠르다
 		}
 
 
+		///
+		///텍스트파일 바이너리 두번 저장
+		///텍스트로 1000을 숫자입력
+		///1000번째 있는 숫자를 제거하기
+		///다시 저장하기
+		///텍스트박스를 하나 생성해서 문자열을 입력하는데 로또번호 10 15 20 35 38 40 입력받아서 찾아서 지우기하고 파일 새로운이름으로 저장
+		///
 
+
+		//목적 : 가장빠르게 파일을 저장하고 원하는데이터를 찾는 방법을 알자
+		// 어떻게하면 파일생성을 빠르게 생성할건가
+		// 어떻게 용량을 적게 할건가
+		// Writeline을 쓰는게 빠른지, format형식을 안쓰는게 빠른지
+		// 1안 => 800만개내용 파일카피해서 프로그램에 나타내는방법
+		//lottoAll2()메소드 만들기 새롭게 파일생성하는방법찾기 그리고 소요시간을 또 쓰기
+		//lottoAll3()메소드 만들기 새롭게 파일생성하는방법찾기 그리고 소요시간을 또 쓰기
+		//1000번째 찾는 로직을 계속 찾기
+
+
+		// 방법 생각 4/1일
 		private void button1_Click(object sender, EventArgs e)
 		{
+			Stopwatch stopwatch = new Stopwatch(); //객체 선언
+			stopwatch.Start(); // 시간측정 시작
 
-			lottoAll();
-			///
-			///텍스트파일 바이너리 두번 저장
-			///텍스트로 1000을 숫자입력
-			///1000번째 있는 숫자를 제거하기
-			///다시 저장하기
-			///텍스트박스를 하나 생성해서 문자열을 입력하는데 로또번호 10 15 20 35 38 40 입력받아서 찾아서 지우기하고 파일 새로운이름으로 저장
-			///
+			lottoFileAll();
+
+
+			stopwatch.Stop(); //시간측정 끝
+			System.Console.WriteLine("time : " + stopwatch.ElapsedMilliseconds / 1000d + "초");
+			label1.Text = stopwatch.ElapsedMilliseconds.ToString(); //레이블1에 보여주기
+
+
+			//using (StreamReader sr = new StreamReader(new FileStream("lotto.txt", FileMode.Open)))
+			//{
+			//	Console.WriteLine($"File size : {sr.BaseStream.Length} bytes");
+			//}
+			//using (BinaryReader br = new BinaryReader(new FileStream("lotto.dat", FileMode.Open)))
+			//{
+			//	Console.WriteLine($"File size : {br.BaseStream.Length} bytes");
+			//}
+
+		}
+
+		private void lottoBinaryAll()
+		{
+
 		}
 
 		private void button2_Click(object sender, EventArgs e)
@@ -137,9 +177,38 @@ namespace WindowsFormsApp7
 			string textValue = lottoString + "\n";
 			// 기존 파일에 text 값을 추가 합니다.
 			System.IO.File.AppendAllText(savePath, textValue, Encoding.Default);
+
+			using (Stream outStream = new FileStream("a.txt", FileMode.Create)) ;
 		}
 
-		private static void binaryAppendData(string filename,  string stringData)
+		//using을 사용하면 자원의 수명을 세부적으로 조절을 할 수 있다.
+		//스트림라이터를 사용해서 쓰기
+		void fileText1(string lottoString)
+		{
+			using (StreamWriter sw = new StreamWriter(new FileStream("lotto.txt", FileMode.Append)))
+			{
+				sw.WriteLine(lottoString);
+			}
+		}
+		//바이너리파일 사용하기
+		//바이너리라이터는 스트림에 이진 데이터를 기록하기 위한 목적으로 쓴다
+		void fileText2(string lottoString)
+		{
+			using (BinaryWriter bw = new BinaryWriter(new FileStream("lotto.dat", FileMode.Append)))
+			{
+				bw.Write(lottoString);
+			}
+		}
+		//시리얼라이
+
+		static void OnWrongPathType(string type)
+		{
+			Console.WriteLine($"{type} is wrong type");
+			return;
+		}
+
+
+		private static void binaryAppendData(string filename, string stringData)
 		{
 			using (var fileStream = new FileStream(filename, FileMode.Append, FileAccess.Write, FileShare.None))
 			using (var bw = new BinaryWriter(fileStream))
@@ -174,6 +243,157 @@ namespace WindowsFormsApp7
 			//BinaryWriter bw = new BinaryWriter(new FileStream("a.dat", FileMode.Create));
 			//bw.Write(lotto);
 			//bw.Close();
+
+
+		}
+
+		private void button2_Click_1(object sender, EventArgs e)
+		{
+			{
+				Stopwatch stopwatch = new Stopwatch(); //객체 선언
+				stopwatch.Start(); // 시간측정 시작
+
+				lottoFileAll2();
+
+				stopwatch.Stop(); //시간측정 끝
+				System.Console.WriteLine("time : " + stopwatch.ElapsedMilliseconds + "ms"); //콘솔로그
+				label1.Text = stopwatch.ElapsedMilliseconds.ToString(); //레이블1에 보여주기
+
+				using (BinaryReader br = new BinaryReader(new FileStream("lotto.dat", FileMode.Open)))
+				{
+					Console.WriteLine($"File size : {br.BaseStream.Length} bytes");
+				}
+
+			}
+		}
+
+		private void lottoFileAll2()
+		{
+			//0번째부터
+			int i = 0;
+			// 넣을 초기값
+			int a = 1;
+			int b = 1;
+			int c = 1;
+			int d = 1;
+			int f = 1;
+			int g = 1;
+			// 6자리 배열 생성
+			int[] array1 = { a, b, c, d, f, g };
+
+			//로또는 1~45 자연수를 가지고 있음
+			//6개를 맞추는거임
+			//45C6 이라는 계산식을 가지고 있음
+			//(45*44*43*42*41*40)/(6*5*4*3*2*1)계산식이 45C6임
+			//for문으로 123456을 가져옴 1 돌리고 2돌리고 3돌려서 마지막 40,41,42,43,44,45만 가지고 오면
+			// 갯수 8145060을 가져옴
+			//Stopwatch stopwatch = new Stopwatch(); //객체 선언
+			//stopwatch.Start(); // 시간측정 시작
+			int index = 0;
+			for (array1[0] = 1; array1[0] <= 40; ++array1[0])
+			{
+				for (array1[1] = array1[0] + 1; array1[1] <= 41; ++array1[1])
+				{
+					for (array1[2] = array1[1] + 1; array1[2] <= 42; ++array1[2])
+					{
+						for (array1[3] = array1[2] + 1; array1[3] <= 43; ++array1[3])
+						{
+							for (array1[4] = array1[3] + 1; array1[4] <= 44; ++array1[4])
+							{
+								for (array1[5] = array1[4] + 1; array1[5] <= 45; ++array1[5])
+								{
+									string lotto = string.Format("{0:#,00}", array1[0]) + string.Format("{0:#,00}", array1[1]) + string.Format("{0:#,00}", array1[2]) + string.Format("{0:#,00}", array1[3])
+											+ string.Format("{0:#,00}", array1[4]) + string.Format("{0:#,00}", array1[5]);
+
+									index++;
+									//Console.WriteLine(index);
+									//텍스트파일저장하기
+									fileText2(lotto);
+									//Console.WriteLine(lotto);
+									i++;
+
+
+								}
+
+
+							}
+						}
+					}
+				}
+			}
+			//stopwatch.Stop(); //시간측정 끝
+			//System.Console.WriteLine("time : " +
+			//stopwatch.ElapsedMilliseconds + "ms");
+			//MessageBox.Show("시간측정 : " + stopwatch.ElapsedMilliseconds + "ms");
+			//8145060
+			Console.WriteLine(i);
+
+
+
+		}
+
+		private void button3_Click_1(object sender, EventArgs e)
+		{
+			BinaryWriter bw = new BinaryWriter(new FileStream("a.dot", FileMode.Create));
+			bw.Write(int.MaxValue);
+			bw.Write("Good Morning");
+			bw.Write(uint.MaxValue);
+			bw.Write("안녕하세요");
+			bw.Write(double.MaxValue);
+			bw.Close();
+
+			BinaryWriter bw1 = new BinaryWriter(new FileStream("b.dot", FileMode.Create));
+			bw1.Write(1);
+			bw1.Close();
+
+			BinaryWriter b1 = new BinaryWriter(new FileStream("b1.dot", FileMode.Create));
+			b1.Write(1);
+			b1.Close();
+
+			BinaryWriter b2 = new BinaryWriter(new FileStream("b2.dot", FileMode.Create));
+			b2.Write(-1);
+			b2.Close();
+
+			BinaryWriter b3 = new BinaryWriter(new FileStream("b3.dot", FileMode.Create));
+			b3.Write(255);
+			b3.Close();
+
+
+
+
+			BinaryWriter bw2 = new BinaryWriter(new FileStream("c.dot", FileMode.Create));
+			bw2.Write("Good Morning");
+			bw2.Close();
+
+			BinaryWriter bw3 = new BinaryWriter(new FileStream("d.dot", FileMode.Create));
+			bw3.Write(uint.MaxValue);
+			bw3.Close();
+
+			BinaryWriter bw4 = new BinaryWriter(new FileStream("e.dot", FileMode.Create));
+			bw4.Write('f');
+			bw4.Close();
+
+			BinaryWriter bw5 = new BinaryWriter(new FileStream("f.dot", FileMode.Create));
+			bw5.Write("fe");
+			bw5.Close();
+
+			byte sa1 = 1;
+			byte[] byte2 = new byte[] { 1, 2, 3, 4, 5, 6 };
+			string string3 = "010203040506";
+
+			BinaryWriter bw7 = new BinaryWriter(new FileStream("gg.dot", FileMode.Create));
+			bw7.Write(byte2);
+			bw7.Close();
+
+			BinaryWriter bw6 = new BinaryWriter(new FileStream("g.dot", FileMode.Create));
+			bw6.Write(string3);
+			bw6.Close();
+
+			fileText1(string3);
+
+			//텍스트는 010102 아스키코드
+
+			//바이너리는 바이트타입으로
 		}
 	}
-		}
+}
